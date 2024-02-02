8aaccea01e6aed5b1721a37e55848e3f85121e09
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-02 07:56:33 +0530

    error handling and renaming things

--- packages/api/llm/mutation.ts
+++ packages/api/llm/mutation.ts
@@ -1,25 +1,35 @@
 import { llm } from ".";
+import { ProductListFormSchema } from "../image-search";
 
 interface InvokeLLMProps<T> {
   stringifiedMetadata: string;
   imageBase64: string;
   stringifiedSampleJsonOutput: string;
 }
+
 export async function invokeLLM<T>({
   stringifiedMetadata: metadata,
   imageBase64: image,
   stringifiedSampleJsonOutput: sampleOutput,
-}: InvokeLLMProps<T>) {
+}: InvokeLLMProps<ProductListFormSchema>) {
   try {
-    const responseString = await llm.invoke({ metadata, image, sampleOutput });
-    const parsedResponse: T = JSON.parse(responseString);
-    console.log(parsedResponse);
+    const responseString = await llm.invoke({
+      metadata,
+      image,
+      sampleJsonOutput: sampleOutput,
+    });
 
-    if (typeof parsedResponse !== "object" || parsedResponse === null) {
-      throw new Error("Invalid JSON format in the response.");
+    try {
+      const parsedResponse: T = JSON.parse(responseString);
+      if (typeof parsedResponse !== "object" || parsedResponse === null) {
+        throw new Error("Invalid JSON format in the response.");
+      }
+      return parsedResponse;
+    } catch (jsonParseError) {
+      throw new Error(
+        `Error parsing JSON: ${(jsonParseError as Error).message}`,
+      );
     }
-
-    return parsedResponse;
   } catch (error) {
     throw new Error((error as Error).message ?? "Error invoking llm");
   }
