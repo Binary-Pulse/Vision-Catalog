a1ad827bc1581c11e5fe0ebfbf2c3ed8dac439ac
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-03 05:10:03 +0530

    exporting the api

---
+++ packages/api/product/index.ts
@@ -0,0 +1,6 @@
+export * from "./images.mutation";
+export * from "./moreDetails.mutation";
+export * from "./price.mutation";
+export * from "./product.mutation";
+export * from "./variants.mutation";
+export * from "./queries";
