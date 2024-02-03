775ffeb0ca115265756ed762b0b88ffbb2eeb3cb
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-04 01:10:20 +0530

    remove separate variant model

--- packages/db/schema/product/variants.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { variantZodSchema } from "../../prisma/zod";
-
-export const addVariantsParams = variantZodSchema.omit({
-  id: true,
-  productId: true,
-});
-export type AddVariantsParamsType = z.infer<typeof addVariantsParams>;
