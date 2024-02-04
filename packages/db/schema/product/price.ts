ce8a8b460e70b20b3f6c8e8337bfe743bc367e80
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-04 14:48:48 +0530

    prettier format

--- packages/db/schema/product/price.ts
+++ packages/db/schema/product/price.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 import { priceZodSchema } from "../../prisma/zod";
 
-export const addPriceParamsSchema = priceZodSchema.omit({ id: true, productId: true });
+export const addPriceParamsSchema = priceZodSchema.omit({
+  id: true,
+  productId: true,
+});
 export type AddPriceParamsType = z.infer<typeof addPriceParamsSchema>;
