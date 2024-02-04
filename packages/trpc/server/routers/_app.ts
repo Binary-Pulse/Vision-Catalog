20c68564e3df21771de37b247b9f8080e6845b25
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-04 14:39:37 +0530

    merging the router

--- packages/trpc/server/routers/_app.ts
+++ packages/trpc/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "../trpc";
 import { imageSearchRouter } from "./image-search";
 import { authRouter } from "./auth";
+import { productRouter } from "./product/_app";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   imageSearch: imageSearchRouter,
+  productRouter: productRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ packages/trpc/server/routers/product/_app.ts
@@ -0,0 +1,14 @@
+import { mergeRouters } from "../../trpc";
+import { brandRouter } from "./brand";
+import { categoryRouter } from "./category";
+import { imagesRouter } from "./images";
+import { moreDetailsRouter } from "./moreDetails";
+import { priceRouter } from "./price";
+
+export const productRouter = mergeRouters(
+  categoryRouter,
+  brandRouter,
+  priceRouter,
+  imagesRouter,
+  moreDetailsRouter,
+);
