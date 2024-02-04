9848650c65cc59eee0b95a05fea418121330cf0d
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-04 16:22:25 +0530

    returning status as well

--- packages/api/product/queries.ts
+++ packages/api/product/queries.ts
@@ -29,27 +29,9 @@ export async function GetUserProductList(userId: Id) {
             images: { take: 1 },
             numberOfItems: true,
             EAN: true,
+            status: true,
           },
         },
-      },
-    });
-    return res;
-  } catch (error) {
-    throw new Error((error as Error).message ?? "INTERNAL_SERVER_ERROR");
-  }
-}
-
-export async function GetCompleteProductDataById(id: Id) {
-  try {
-    const res = await db?.product.findFirst({
-      where: { id },
-      include: {
-        brand: true,
-        images: true,
-        price: true,
-        moreDetails: true,
-        category: true,
-        variant: true,
       },
     });
     return res;
