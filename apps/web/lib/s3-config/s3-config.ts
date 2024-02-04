40058d6a2b5eae5b2f242c0ee9101018d3b65056
Somnath Golui <code.somu@gmail.com>
2024-02-04 13:41:21 +0530

    Formated the file

--- apps/web/lib/s3-config/s3-config.ts
+++ apps/web/lib/s3-config/s3-config.ts
@@ -1,18 +1,22 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
     accessKeyId: "",
-    secretAccessKey: ""
-  }
+    secretAccessKey: "",
+  },
 });
 
 async function getObjectURL(key: string) {
   const command = new GetObjectCommand({
     Bucket: "code-my-uploads",
-    Key: key
+    Key: key,
   });
   const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
   return url;
@@ -22,7 +26,7 @@ async function putObject(filename: string, contentType: string) {
   const command = new PutObjectCommand({
     Bucket: "code-my-uploads",
     Key: `uploads/user-uploads/${filename}`,
-    ContentType: contentType
+    ContentType: contentType,
   });
   const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
   return url;
