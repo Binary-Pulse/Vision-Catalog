6ca3a9aa734165e02907f34816aca80c5d3f55d0
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-04 19:32:56 +0530

    error fix

--- apps/web/app/[locale]/(lobby)/layout.tsx
+++ apps/web/app/[locale]/(lobby)/layout.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import { ProfileHeader } from "@/components/layout/profile-header";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
-import { VoiceInput } from "@/components/layout/VoiceInput";
 
 export default function LobbyLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col min-h-screen">
       <SiteHeader>
         <ProfileHeader />
-        <VoiceInput />
+        {/* <VoiceInput /> */}
       </SiteHeader>
       <main className="flex-1">{children}</main>
       <SiteFooter></SiteFooter>
