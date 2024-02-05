bcc2c293178d54f9e1fc1e923ea14a3efae54378
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-05 06:36:48 +0530

    seperate schema and class creator for text and image

---
+++ packages/api/vector-search/internal/class-schema-creator.ts
@@ -0,0 +1,30 @@
+import { client } from "..";
+import { imageSchemaConfig, textSchemaConfig } from "../schemaConfig";
+
+export async function imageSeachClassCreator() {
+  try {
+    const data = await client.schema
+      .classCreator()
+      .withClass(imageSchemaConfig)
+      .do();
+    return { msg: "Image Class created successfuly", data };
+  } catch (error) {
+    throw new Error(
+      (error as Error).message ?? "INTERNAL_SERVER_ERROR- Error creating Class",
+    );
+  }
+}
+
+export async function textSearchClassCreator() {
+  try {
+    const data = await client.schema
+      .classCreator()
+      .withClass(textSchemaConfig)
+      .do();
+    return { msg: "Text Class created successfuly", data };
+  } catch (error) {
+    throw new Error(
+      (error as Error).message ?? "INTERNAL_SERVER_ERROR- Error creating Class",
+    );
+  }
+}
--- packages/api/vector-search/schemaConfig.ts
+++ packages/api/vector-search/schemaConfig.ts
@@ -1,26 +1,40 @@
-export function genSchema(className: string) {
-  const schemaConfig = {
-    class: className,
-    vectorizer: "img2vec-neural",
-    vectorIndexType: "hnsw",
-    moduleConfig: {
-      "img2vec-neural": {
-        imageFields: ["image"],
-      },
+import { SEARCH_BY_IMAGE_CLASS, SEARCH_BY_TEXT_CLASS } from "@repo/utils";
+
+export const imageSchemaConfig = {
+  class: SEARCH_BY_IMAGE_CLASS,
+  vectorizer: "img2vec-neural",
+  vectorIndexType: "hnsw",
+  moduleConfig: {
+    "img2vec-neural": {
+      imageFields: ["image"],
     },
-    properties: [
-      {
-        name: "image",
-        dataType: ["blob"],
-      },
-      {
-        name: "metadata",
-        dataType: ["text"],
-      },
-    ],
-  };
-  return schemaConfig;
-}
+  },
+  properties: [
+    {
+      name: "image",
+      dataType: ["blob"],
+    },
+    {
+      name: "metadata",
+      dataType: ["text"],
+    },
+  ],
+};
+
+export const textSchemaConfig = {
+  class: SEARCH_BY_TEXT_CLASS,
+  vectorizer: "text2vec-palm",
+  vectorIndexType: "hnsw",
+  moduleConfig: {
+    "text2vec-palm": {},
+  },
+  properties: [
+    {
+      name: "metadata",
+      dataType: ["text"],
+    },
+  ],
+};
 
 export const sampleProductListForm = {}; // the type of this will be exact type of form
 export type ProductListFormSchema = typeof sampleProductListForm;
