58976370523ed24dbce99fa2a833522e4fb227ce
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-06 03:49:46 +0530

    update metadata by vectorDBobjectIds and productId

--- packages/api/product/metadata.ts
+++ packages/api/product/metadata.ts
@@ -7,6 +7,7 @@ import {
 import {
   ProductSearchVectorSchema,
   addNewProductMetadata,
+  updateProductMetadata,
 } from "../vector-search";
 
 export async function addMetadataToVectorDB(productId: Id) {
@@ -47,6 +48,62 @@ export async function addMetadataToVectorDB(productId: Id) {
     product.images?.primaryImageUrl as string,
   );
   const vectorProductMetadataResponse = await addNewProductMetadata({
+    imageBase64,
+    imageClassName: SEARCH_BY_IMAGE_CLASS,
+    textClassName: SEARCH_BY_TEXT_CLASS,
+    metadata,
+  });
+  return vectorProductMetadataResponse;
+}
+interface updateMetadataToVectorDBProps {
+  productId: Id;
+  imageClassPropertyId: string;
+  textClassPropertyId: string;
+}
+export async function updateMetadataToVectorDB({
+  productId,
+  imageClassPropertyId,
+  textClassPropertyId,
+}: updateMetadataToVectorDBProps) {
+  const product = await db?.product.findFirstOrThrow({
+    where: { id: productId },
+    include: {
+      brand: { select: { name: true } },
+      category: { select: { name: true } },
+      images: { select: { primaryImageUrl: true } },
+      price: { select: { currency: true, ppu: true } },
+    },
+  });
+  if (!product) {
+    throw new Error("PRODUCT_NOT_FOUND");
+  }
+  const primaryImageUrl = product?.images?.primaryImageUrl;
+  const currency = product?.price?.currency as "INR" | "USD";
+  const pricePerUnit = product?.price?.ppu as number;
+  const metadata: ProductSearchVectorSchema = {
+    productId: productId,
+
+    productName: product.productName,
+    description: product.description,
+    ean: product.EAN,
+    isbn: product.ISBN,
+    sku: product.SKU,
+    searchTerms: product.searchTerms,
+    upc: product.UPC,
+
+    brand: product?.brand?.name,
+    category: product?.category?.name,
+
+    primaryImageUrl,
+    currency,
+    pricePerUnit,
+  };
+  const imageBase64 = await imageURLToBase64(
+    product.images?.primaryImageUrl as string,
+  );
+  const vectorProductMetadataResponse = await updateProductMetadata({
+    imageClassPropertyId,
+    textClassPropertyId,
     imageBase64,
     imageClassName: SEARCH_BY_IMAGE_CLASS,
     textClassName: SEARCH_BY_TEXT_CLASS,
