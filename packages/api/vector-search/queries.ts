9f2ed67e5dd21be718daef9495b6f9b48cd0066d
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-05 06:35:18 +0530

    product metadata retrievers

--- packages/api/vector-search/queries.ts
+++ packages/api/vector-search/queries.ts
@@ -1,19 +1,44 @@
 import { ProductSearchVectorSchema, client } from ".";
 
-interface ImageProductVectorRetrieverProps {
+interface ImageVectorMetadataRetrieverProps {
   className: string;
   image: string;
 }
-export async function ImageProductVectorRetriever({
+export async function ImageVectorMetadataRetriever({
   className,
   image,
-}: ImageProductVectorRetrieverProps) {
+}: ImageVectorMetadataRetrieverProps) {
   try {
     const res = await client.graphql
       .get()
       .withClassName(className)
       .withFields("metadata")
       .withNearImage({ image })
+      // .withLimit(1)
+      .do();
+    const responseArray = res.data.Get[className] as string[];
+    const metadataArray: ProductSearchVectorSchema[] = responseArray.map(
+      (response) => JSON.parse(response),
+    );
+    return metadataArray;
+  } catch (error) {
+    throw new Error((error as Error).message ?? "INTERNAL_SERVER_ERROR");
+  }
+}
+interface TextVectorMetadataRetrieverProps {
+  className: string;
+  text: string;
+}
+export async function TextVectorMetadataRetriever({
+  className,
+  text,
+}: TextVectorMetadataRetrieverProps) {
+  try {
+    const res = await client.graphql
+      .get()
+      .withClassName(className)
+      .withFields("metadata")
+      .withNearText({ concepts: [text] })
       // .withLimit(1)
       .do();
     const responseArray = res.data.Get[className] as string[];
