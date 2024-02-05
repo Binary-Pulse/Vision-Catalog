f8e56bb23ca46a504ce4eba030b74574fe7b7dc0
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-06 02:22:47 +0530

    addImages api removed using update images instead

--- packages/api/product/images.mutation.ts
+++ packages/api/product/images.mutation.ts
@@ -4,32 +4,28 @@ interface AddImagesToProductProps {
   productId: Id;
   imagesData: AddImagesParamsType;
 }
-export async function AddImagesToProduct({
-  productId,
-  imagesData,
-}: AddImagesToProductProps) {
-  try {
-    await db?.image.create({
-      data: { ...imagesData, product: { connect: { id: productId } } },
-    });
-    return { msg: "Images Added Successfully" };
-  } catch (error) {
-    throw new Error((error as Error).message ?? "INTERNAL_SERVER_ERROR");
-  }
-}
+// export async function AddImagesToProduct({
+//   productId,
+//   imagesData,
+// }: AddImagesToProductProps) {
+//   try {
+//     await db?.image.create({
+//       data: { ...imagesData, product: { connect: { id: productId } } },
+//     });
+//     return { msg: "Images Added Successfully" };
+//   } catch (error) {
+//     throw new Error((error as Error).message ?? "INTERNAL_SERVER_ERROR");
+//   }
+// }
 
 export async function UpdateImagesForProduct({
   productId,
   imagesData,
 }: AddImagesToProductProps) {
   try {
-    const existingProduct = await db?.product.findUnique({
+    await db?.product.findFirstOrThrow({
       where: { id: productId },
     });
-
-    if (!existingProduct) {
-      throw new Error("PRODUCT_NOT_FOUND");
-    }
 
     await db?.image.update({
       where: { productId: productId },
