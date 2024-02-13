b570a6e7f66439f699dcb3e7b5ce0bfb9189a7b4
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-13 08:25:41 +0530

    removing llm

--- packages/trpc/server/routers/vision.ts
+++ packages/trpc/server/routers/vision.ts
@@ -1,14 +1,8 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
-import { invokeLLM } from "@repo/api/llm";
-import {
-  ImageVectorMetadataRetriever,
-  ProductListFormSchema,
-  sampleProductListForm,
-} from "@repo/api/vector-search";
-import { SEARCH_BY_IMAGE_CLASS, imageURLToBase64 } from "@repo/utils";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
 export const visionProRouter = createTRPCRouter({
-  vision: publicProcedure
+  vision: protectedProcedure
     .meta({
       /* 👉 */ openapi: {
         method: "POST",
@@ -22,13 +16,5 @@ export const visionProRouter = createTRPCRouter({
       }),
     )
     .output(z.object({}))
-    .mutation(async ({ input }) => {
-      const image = await imageURLToBase64(input.imageURL);
-      const stringifiedSampleJsonOutput = JSON.stringify(sampleProductListForm);
-      const response = await invokeLLM({
-        imageBase64: image,
-        stringifiedSampleJsonOutput,
-      });
-      return response;
-    }),
+    .mutation(async ({ input, ctx: { userId } }) => {}),
 });
