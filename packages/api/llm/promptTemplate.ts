98b867bddaadbb411be7c720789e3ae836ad8b64
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-02-13 08:25:33 +0530

    removing llm

--- packages/api/llm/promptTemplate.ts
+++ packages/api/llm/promptTemplate.ts
@@ -1,43 +1,40 @@
 import { PromptTemplate } from "langchain/prompts";
 
-export const instruction = `You're a powerful language model with the ability to extract data from product images.\n 
-Your task ahead involves refining product information using two things:\n 
-the actual product image, \n
-and the sample output that you need to provide.\n 
-Use the provided image to add details to the json output, who's schema will be provided soon.\n 
-You can tweak descriptions or bullet points for clarity, but keep the main details intact.\n
-Stick to the plan (schema), fix things if they seem off, and remember, if some info is missing – that's alright.\n
-Output a JSON string matching the provided schema.\n 
-If uncertain from the image, use null there as expected.\n 
+export const promptTemplate = PromptTemplate.fromTemplate(
+  `You're a powerful language model with the ability to extract data from product images.\n 
+  Your task ahead involves refining product information using the product image, \n
+  Use the provided image to add details to the json output, who's schema will be provided soon.\n 
+  Just return the information in a very opinioted fashion so that it can be parsed easily string in the specified format and don't make things up.\n
+  Our main goal is to help sellers fill in their details in the form for product listing, so you can't make things up.
+  Note: Only process images that are appropriate for a product context. 
+  If the image is explicit, sexual, or unrelated to products, 
+  return an appropriate message indicating that the image doesn't meet the required criteria.\n 
+  Good luck!
+  Image: This is the image you need to perform tasks. 
+  [Image](data:image/png;base64,{image})  
+  
+  Use this sample schema to understand in what informations you need to give in your output.
+  Sample Output: 
+  {sampleOutput}\n\n
+      `,
+);
+const instruction2 = `
+  You are a powerful language model whose task is to generate a json output based on provided schema,
+  and all the details that needs to be filled in that schema based on the context provided and google search data provided.
+
+  Output a JSON string matching the provided schema.\n 
+If uncertain from the any field in schema, use null there as expected.\n 
 Just return that JSON string in the specified format and don't make things up.\n
 Our main goal is to help sellers fill in their details in the form for product listing, 
-your JSON output will be parse using JSON.parse(yourOutput) and send to the fronted to fill in the form fields, 
+your stringified JSON output will be parsed using JSON.parse(yourOutput) and send to the fronted to fill in the form fields, 
 so you can't make things up.
 so focus on making the info better without making things up.\n 
-Note: Only process images that are appropriate for a product context. 
-If the image is explicit, sexual, or unrelated to products, 
+If the image is explicit, sexual, or unrelated to products or the data says explicit content or something then, 
 return an appropriate message WHICH SHOULD NOT BE IN THE FORM OF JSON indicating that the image doesn't meet the required criteria.\n 
-Good luck!`;
-
-export const promptTemplate = PromptTemplate.fromTemplate(
-  `${instruction}\n
-      More Instruciton: For example, if the description can be improved or if additional bullet points can be added for clarity, feel free to enhance those fields. 
-      However, be cautious not to distort the essential information.
-      Handling Empty or Incomplete/Empty Fields: Skillfully handle cases where fields might be empty or incomplete, 
-      that is the information could not have been retrieved from the image. 
-      If certain fields are missing or unavailable, 
-      provide context you retrieved from the image or place null in the JSON schema of the output. 
-      Do not force the inclusion of information that is not available.
-      At the end you'll have to return a stringifiedJson which will be parsed and its value will be placed in form.
-      So your output will be identical to the sample Output i gave but with the newer values and null values (if could not retrieve from image or from your knowledge) 
-      for fields after retiriving information from the image.
-      return an appropriate message which should not be in the form of json indicating that the image doesn't meet the required criteria.
-
-      Image: This is the image you need to perform tasks. 
-      [Image](data:image/png;base64,{image})  
-      
-      Use this sample output JSON to understand in what way you need to give your output.
-      Sample Output: 
-      {sampleOutput}\n\n
-      `,
+Just say something like inappropriate explicit content or unrelated to product.
+`;
+export const promptTemplate2 = PromptTemplate.fromTemplate(
+  `${instruction2}\n
+  You are powerful 
+  `,
 );
